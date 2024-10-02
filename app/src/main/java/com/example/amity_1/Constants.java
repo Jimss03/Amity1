49354f2b3d3c5aa6c11207558418bf8de6607f09
Jimuel Destura <92593189+Jimss03@users.noreply.github.com>
2024-10-03 01:25:51 +0800

    Initial commit

---
+++ app/src/main/java/com/example/amity_1/Constants.java
@@ -0,0 +1,10 @@
+package com.example.amity_1;
+
+public class Constants {
+
+    public static final String BASE_URL = "https://cornflowerblue-quetzal-932463.hostingersite.com/";
+    public static final String PREFERENCE_NAME = "session";
+    public static final String KEY_ISE_LOGGED_IN = "isLoggedIn";
+    public static final String KEY_EMAIL = "email";
+    public static final String KEY_USERNAME = "username";
+}
